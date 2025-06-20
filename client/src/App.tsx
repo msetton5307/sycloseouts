bec4e8a0c1d636dbcb74a6cb61271f58a5aa82f3
msetton5307 <165737951+msetton5307@users.noreply.github.com>
2025-06-20 15:52:17 -0400

    feat(admin): allow message supervision

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -32,6 +32,7 @@ import AdminUsers from "@/pages/admin/users";
 import AdminApplications from "@/pages/admin/applications";
 import HelpPage from "@/pages/help-page";
 import AdminTicketsPage from "@/pages/admin/tickets";
+import AdminMessagesPage from "@/pages/admin/messages";
 import AboutPage from "@/pages/about-page";
 import SellerAgreementPage from "@/pages/seller-agreement";
 import BuyerAgreementPage from "@/pages/buyer-agreement";
@@ -78,6 +79,7 @@ function Router() {
       <ProtectedRoute path="/admin/users" component={AdminUsers} allowedRoles={["admin"]} />
       <ProtectedRoute path="/admin/applications" component={AdminApplications} allowedRoles={["admin"]} />
       <ProtectedRoute path="/admin/featured" component={FeaturedProductsPage} allowedRoles={["admin"]} />
+      <ProtectedRoute path="/admin/messages" component={AdminMessagesPage} allowedRoles={["admin"]} />
       <ProtectedRoute path="/admin/tickets" component={AdminTicketsPage} allowedRoles={["admin"]} />
 
       {/* Fallback to 404 */}
