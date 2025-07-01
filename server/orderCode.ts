36debebc078cac95675a56af580ea11ddc1feb48
msetton5307 <165737951+msetton5307@users.noreply.github.com>
2025-07-01 14:46:59 -0400

    feat: lengthen order code

--- server/orderCode.ts
+++ server/orderCode.ts
@@ -1,5 +1,7 @@
+import { randomBytes } from "crypto";
+
 export function generateOrderCode(id: number): string {
-  const multiplier = 9973; // prime number
-  const offset = 12345;
-  return 'O' + ((id * multiplier + offset).toString(36).toUpperCase());
+  const idPart = id.toString(36).toUpperCase().padStart(6, "0");
+  const randomPart = randomBytes(3).toString("hex").toUpperCase();
+  return `O${idPart}${randomPart}`;
 }
