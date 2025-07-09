137ede2c4a25298b338f93f1e8ed383d8ebd82d5
msetton5307 <165737951+msetton5307@users.noreply.github.com>
2025-07-09 13:48:36 -0400

    Constrain logo height

--- client/src/components/layout/header-fixed.tsx
+++ client/src/components/layout/header-fixed.tsx
@@ -50,7 +50,11 @@ export default function Header() {
               <div className="flex-shrink-0 flex items-center">
                 <Link href="/">
                   {settings?.logo ? (
-                    <img src={settings.logo} alt="Logo" className="h-8 w-auto" />
+                    <img
+                      src={settings.logo}
+                      alt="Logo"
+                      className="h-full max-h-16 w-auto object-contain"
+                    />
                   ) : (
                     <span className="text-primary font-bold text-2xl cursor-pointer">SY Closeouts</span>
                   )}
